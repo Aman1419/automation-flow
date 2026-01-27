63ea1ec50a7b09403e3498e1094e612725a62932
Aman Yadav <aman.yadav@nvest.in>
2026-01-27 22:55:08 +0530

    added multiply logic

--- test.js
+++ test.js
@@ -1,2 +1,13 @@
 // this is the test file for the code review part :- 
 console.log("testing code review file test file")
+
+// added function to multiply
+let a = 10;
+let b = 20;
+let result = null;
+
+function multiply(a,b){
+   result = a * b;
+}
+
+console.log(multiply(a,b));
