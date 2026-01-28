88a378adeb38c62e1ebaed9de23a99c53406921f
Aman Yadav <aman.yadav@nvest.in>
2026-01-28 18:58:48 +0530

    added sorting logic

--- test.js
+++ test.js
@@ -1,24 +1,43 @@
-// this is the test file for the code review part :- 
-console.log("testing code review file test file")
+// this is the test file for the code review part :-
+console.log("testing code review file test file");
 
 // added function to multiply
 let a = 10;
 let b = 20;
-let result = null;
 
-function multiply(a,b){
-  return result = a * b;
+function multiply(a, b) {
+  return a * b;
 }
 
-console.log(multiply(a,b));
-
+console.log(multiply(a, b));
 
 // added functionality for the reverse a string
 let givenString = "Aman";
 
-function reverseString(str){
-let reversed = str.split('').reverse().join();
-return reversed
+function reverseString(str) {
+  let reversed = str.split("").reverse().join("");
+  return reversed;
 }
 
-console.log(reverseString(givenString))
+console.log(reverseString(givenString));
+
+
+
+// function for the sorting 
+function bubbleSort(arr) {
+  const a = [...arr]; // copy to avoid mutation
+  const n = a.length;
+
+  for (let i = 1; i < n; i++) {
+    for (let j = i +1; j < n - i - 1; j++) {
+      if (a[j] > a[j + 1]) {
+        const temp = a[j];
+        a[j] = a[j + 1];
+        a[j + 1] = temp;
+      }
+    }
+  }
+  return a;
+}
+
+console.log(bubbleSort([10, 2, 5, 1, 8]));
