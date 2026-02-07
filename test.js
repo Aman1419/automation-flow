9e7318f0c13b3a3710575b1e9f4bb538fc338351
Aman Yadav <aman.yadav@nvest.in>
2026-02-07 15:01:38 +0530

    added factorial function

--- test.js
+++ test.js
@@ -56,4 +56,17 @@ function add (a, b)  {
   a + b
 }
 
-add(5,10)
+add(5,10);
+
+// function to find the factorial of a number
+function factorial(n) {
+  if (n < 0) return "cannot be less than 0";
+  if (n === 0 || n === 1) return 1;
+  let result = 0;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+console.log(factorial(5));
